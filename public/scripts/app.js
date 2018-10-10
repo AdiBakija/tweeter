7362ec848b78fe6e9e99dff6cb8c11459eaac04b
Adi <adibakija35@hotmail.com>
2018-10-10 21:34:56 +0000

    Created loadTweets function which renders all tweets from the /tweets route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,54 +6,7 @@
 
 $(document).ready(function() {
 
-  // Fake data taken from tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
+  //Helper function to render tweet list
   function renderTweets(tweets) {
     // loops through tweets
     for (index of tweets) {
@@ -64,8 +17,7 @@ $(document).ready(function() {
       $('#tweets-container').append($tweet);
     }
 
-  }
-
+  };
 
   // Helper function that constructs NEW tweet elements based on DOM.
   function createTweetElement (tweetData) {
@@ -89,10 +41,15 @@ $(document).ready(function() {
       </article>
       `;
 
-  }
-  // Call renderTweets function and pass raw data (array of objects from above).
-  // At some point the data passed will be an actual dynamic database of tweets.
-  renderTweets(data);
+  };
+
+  //Function to load the list of all tweets inside of the /tweets database
+  function loadTweets () {
+
+    $.getJSON( "/tweets", function (data) {
+      renderTweets(data);
+    });
+  };
 
   // Submit form handler for post requests
   $("#submit-tweet").submit(function(event) {
@@ -106,4 +63,8 @@ $(document).ready(function() {
     $.post("/tweets/", $serialized);
 
   });
+
+  //Call load tweets to populate homepage with list of tweets
+  loadTweets();
+
 });
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -39,7 +39,7 @@ module.exports = function(DataHelpers) {
         res.status(201).send();
       }
     });
-    console.log(req.body);
+
   });
 
   return tweetsRoutes;
